f212ca72da3253cd317d659df664237ced6aa3e7
Brockaaa <brocka.dev@gmail.com>
2023-01-26 16:19:42 +0100

    26.01.23 16:19:42

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -44,7 +44,7 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("/beegfs/ws/1/anbr392b-test-workspace/argupta-vdi_generation/DistributedVis/build/TestLibrary/cmake_install.cmake")
+  include("/scratch/ws/1/anbr392b-test-workspace/argupta-vdi_generation/DistributedVis/build/TestLibrary/cmake_install.cmake")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -55,5 +55,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/beegfs/ws/1/anbr392b-test-workspace/argupta-vdi_generation/DistributedVis/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/scratch/ws/1/anbr392b-test-workspace/argupta-vdi_generation/DistributedVis/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")

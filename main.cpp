90e0368ee06d3432392e8a39bd6f7cc5c2564147
Brockaaa <brocka.dev@gmail.com>
2023-03-04 13:39:36 +0100

    04.03.23 13:39:36

--- main.cpp
+++ main.cpp
@@ -256,7 +256,7 @@ int main(int argc, char** argv) {
     }
 
     if(true) {
-        int * volume_dimensions = getVolumeDims(getEnvVar("DATASET_PATH") + "/" + dataset + "/Part1");
+        int * volume_dimensions = getVolumeDims(getEnvVar("DATASET_PATH") + "/" + dataset);
         //int * volume_dimensions = getVolumeDims("/beegfs/ws/1/argupta-vdi_generation/Datasets/"+ dataset);
         float pixelToWorld = 3.84f / (float)volume_dimensions[0]; //empirical
         setDatasetParams(jvmData, dataset, pixelToWorld, volume_dimensions);
